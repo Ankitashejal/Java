1fd4e7df142e63494724d062faec781a4e630346
Ankita Shejal <ankitashejal11@gmail.com>
2023-07-01 22:57:22 +0530

    star patterns

---
+++ halfpyramid2.java
@@ -0,0 +1,21 @@
+public class halfpyramid2 {
+    public static void inverted_rotated(int n){
+        for(int i=1;i<=n;i++){
+            //spaces
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+               }
+               //stars
+                for(int k=1;k<=i;k++){
+                  System.out.print("*" );  
+                
+            }
+            System.out.println();
+        }
+
+    }
+    public static void main(String args[]){
+        inverted_rotated(4);
+    }
+    
+}
