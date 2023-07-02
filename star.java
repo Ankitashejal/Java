55cc414f2c6821706b356e97a1c82874fff7ab12
Ankita Shejal <ankitashejal11@gmail.com>
2023-07-02 15:16:57 +0530

    changes

--- star.java
+++ star.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 public class star {
     public static void main(String args[]){
@@ -11,16 +10,3 @@ public class star {
         }
 }
 
-=======
-public class star {
-    public static void main(String args[]){
-         for(int line=1; line<=4; line++){
-            for(int star=1; star<=line; star++){
-                System.out.print("*");
-            }
-            System.out.println();
-         }
-        }
-}
-
->>>>>>> e9f9d7ff08feda76f28f76469a2266881a30990c
