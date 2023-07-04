4ec56b345930946ca01f3c754e38cc46db4f65fa
Ankita Shejal <ankitashejal11@gmail.com>
2023-07-04 22:04:53 +0530

    update

--- Advanced_patterns/Palindrome_triangle.java
+++ Advanced_patterns/Palindrome_triangle.java
@@ -3,12 +3,15 @@ package Advanced_patterns;
 public class Palindrome_triangle {
     public static void star(int n){
         for(int i=1;i<=n;i++){
+        //inner loop to print the spaces
         for(int j=1;j<=2*(n-i);j++){
             System.out.print(" ");
         }
+        //to print 1st part
         for(int j=i;j>=1;j--){
             System.out.print(j + " ");
         }
+        //to print 2nd part
         for(int j=2;j<=i;j++){
             System.out.print(j + " ");
         }
--- hollow_rectangle.java
+++ hollow_rectangle.java
@@ -12,12 +12,8 @@ public static void star (int totRows,int totCols) {
        }
       }
     public static void main(String args[]) {
-<<<<<<<< HEAD:hollow_rectangle.java
-      star (4,5);
-========
-     star (4,5);
->>>>>>>> 9672219f337ca1349b5fbbe01b34f167e835ecb5:5_hollow_rectangle.java
 
+      star (4,5);
     }
 
 }
