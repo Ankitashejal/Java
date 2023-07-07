622754050bf844113140585c7bcfd13de62d9c3a
Ankita Shejal <ankitashejal11@gmail.com>
2023-07-07 22:32:09 +0530

    new

--- BubbleSorting/Bubblesort.java
+++ BubbleSorting/Bubblesort.java
@@ -1,10 +1,9 @@
-import java.util.*;
-public class BasicSorting {
+package BubbleSorting;
+
+public class Bubblesort {
     public static void bubbleSort(int arr[]){
-        
-        
-        for (int turn = 0; turn < arr.length-1;turn++){
-           (for int j=0; j<arr.length-1-turn; j++){
+         for (int turn = 0; turn <= arr.length-2;turn++){
+           for( int j=0; j<=arr.length-2-turn; j++){
             if (arr[j]> arr[j+1]){
                 //swap
                 int temp = arr[j];
@@ -12,17 +11,22 @@ public class BasicSorting {
                 arr[j+1] = temp;
             }
            }
-        
+        }
     }
-    public static void printArr(int arr[]){
-        for(int i= 0; i<arr.length ;i++){
+
+    public static void printArr(int arr[]) {
+ for(int i= 0; i<arr.length ;i++){
             System.out.print(arr[i]+"");
         }
         System.out.println();
     }
-    public static void main(String args[]){
-        int arr = {5,4,1,3,2};
+     public static void main(String args[]){
+        int arr[] = {5,4,1,3,2};
         bubbleSort(arr);
         printArr(arr);
     }
+
+    
+    
+
 }
