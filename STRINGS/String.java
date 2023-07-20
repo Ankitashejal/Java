b26962d9df51dd2b3781a6453c9e5c523734f210
Ankita Shejal <ankitashejal11@gmail.com>
2023-07-20 10:24:58 +0530

    main string

---
+++ STRINGS/String.java
@@ -0,0 +1,13 @@
+package STRINGS;
+
+public class String {
+    public static void main(java.lang.String[] args) {
+	    java.lang.String firstName = "ankita";
+        java.lang.String lastName  = "shejal";
+        java.lang.String fullName = firstName+ " " +lastName;
+        System.out.println(fullName.charAt(0));	
+
+}
+}
+  
+
