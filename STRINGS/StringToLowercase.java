527bc0cb79448c7e69439524c21ebee6d47941f7
Ankita Shejal <ankitashejal11@gmail.com>
2023-07-21 21:59:46 +0530

    string

---
+++ STRINGS/StringToLowercase.java
@@ -0,0 +1,10 @@
+package STRINGS;
+
+public class StringToLowercase {
+    public static void main(String args[])
+    {
+        java.lang.String s = "My Name is Ankita";
+        java.lang.String s2 = s.toLowerCase();
+        System.out.println(s2);
+    }
+}
