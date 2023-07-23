58dee3aeec3425d0d34b04f18ed5b7017641a94c
Ankita Shejal <ankitashejal11@gmail.com>
2023-07-23 09:06:35 +0530

    string

---
+++ STRINGS/SubString.java
@@ -0,0 +1,10 @@
+package STRINGS;
+
+public class SubString {
+   
+    public static void main(String[] args) {
+        String str = new String("Hello World");
+        System.out.print("The extracted substring is : ");         
+        System.out.println(str.substring(str, 0, 5));
+    }
+}
