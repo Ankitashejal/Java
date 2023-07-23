9cbcba22eb4abd02bbf41b75b90499090f454d20
Ankita Shejal <ankitashejal11@gmail.com>
2023-07-23 09:06:45 +0530

    string

--- STRINGS/StringToLowercase.java
+++ STRINGS/StringToLowercase.java
@@ -1,10 +1,9 @@
 package STRINGS;
 
 public class StringToLowercase {
-    public static void main(String args[])
-    {
-        java.lang.String s = "My Name is Ankita";
-        java.lang.String s2 = s.toLowerCase();
-        System.out.println( java.lang.String s2);
+    public static void main(String args[]){
+    java.lang.String a= "ANKITA";
+       java.lang.String b= "Shejal";
+       System.out.println(a.toLowerCase());
     }
 }
---
+++ STRINGS/StringToUppercase.java
@@ -0,0 +1,12 @@
+package STRINGS;
+
+public class StringToUppercase {
+    public static void main(String args[])
+    {
+        java.lang.String a= "ANKITA";
+        java.lang.String b= "Shejal";
+        System.out.println(b.toUpperCase());
+     }
+       
+    }
+
