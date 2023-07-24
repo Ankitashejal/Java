b07fcb4ab02eaa42748922fa47bc8a72390d3915
Ankita Shejal <ankitashejal11@gmail.com>
2023-07-24 20:58:41 +0530

    string

---
+++ STRINGS/index.java
@@ -0,0 +1,10 @@
+package STRINGS;
+
+public class index {
+    public static void main(String args[])
+    {
+        java.lang.String a= "ANKITA";
+        java.lang.String b= "Shejal";
+        System.out.println(a.indexOf('A'));
+     }
+}
