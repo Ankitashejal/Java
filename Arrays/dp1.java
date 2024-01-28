f9c9bb91230a1610dd081e5bb564c25c629130f3
Ankita <Ankitashejal11@gmail.com>
2024-01-28 20:55:49 +0530

    updated

--- Arrays/dp1.java
+++ Arrays/dp1.java
@@ -2,7 +2,12 @@ package Arrays;
 
 class dp1{
     public static void main(String[] args) {
-        int numbers[] = {2, 3,4,5,6,7,8};
+        int numbers[] = {2,3,4,5,6,7,8};
+        reverse(numbers);
+        for(int i = 0; i<numbers.length ; i++){
+            System.out.print(numbers[i]+ " ");
+        }
+        System.out.println();
     }
  static void reverse( int numbers[]){
     int first = 0;
