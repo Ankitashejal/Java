e3f02ce36afb37baebfd5829d07be692e30c3843
Ankita <Ankitashejal11@gmail.com>
2024-02-20 15:42:35 +0530

    oops concepts

--- Arrays/dp1.java
+++ Arrays/dp1.java
@@ -1,23 +1,43 @@
 package Arrays;
 
+// class dp1{
+//     public static void main(String[] args) {
+//         int numbers[] = {2,3,4,5,6,7,8};
+//         reverse(numbers);
+//         for(int i = 0; i<numbers.length ; i++){
+//             System.out.print(numbers[i]+ " ");
+//         }
+//         System.out.println();
+//     }
+//  static void reverse( int numbers[]){
+//     int first = 0;
+//     int last = numbers.length-1;
+//     while(first<last){
+//         int temp = numbers[first];
+//         numbers[first] = numbers[last];
+//         numbers[last] = temp;
+//         first++;
+//         last--;
+//     }
+//  }
+// }
+
+//pairsinarray
 class dp1{
+ 
     public static void main(String[] args) {
-        int numbers[] = {2,3,4,5,6,7,8};
-        reverse(numbers);
-        for(int i = 0; i<numbers.length ; i++){
-            System.out.print(numbers[i]+ " ");
+        int numbers[] = {2,4,6,8,10};
+        pairs(numbers);
+    }
+        
+    public static void pairs(int numbers[]) {
+        for(int i = 0;i<numbers.length;i++){
+            int curr=numbers[i];
+            for(int j=i+1;j<numbers.length;j++){
+                System.out.print("(" + curr + "," + numbers[j] + ")");
+                }
+                System.out.println();
+            }
         }
-        System.out.println();
     }
- static void reverse( int numbers[]){
-    int first = 0;
-    int last = numbers.length-1;
-    while(first<last){
-        int temp = numbers[first];
-        numbers[first] = numbers[last];
-        numbers[last] = temp;
-        first++;
-        last--;
-    }
- }
-}
+
--- OOPS/oops.java
+++ OOPS/oops.java
@@ -1,23 +1,38 @@
-public class OOPS{
-    public static void main(String args[]){
-    Pen p1 = new Pen();//created a pen object called p1
-    p1.setColor("Blue");
-    System.Out.Println(p1.Color);
-    p1.setTip(5);
-    System.Out.Println(p1.Tip);
-    }
+public class oops{
+//     public static void main(String args[]){
+//     Pen p1 = new Pen();//created a pen object called p1
+//     p1.setColor("Blue");
+//     System.Out.Println(p1.Color);
+//     p1.setTip(5);
+//     System.Out.Println(p1.Tip);
+//     }
+// }
+
+// class Pen{
+//     string Color;
+//     int Tip;
+
+//     void setColor(String newColor){
+//         color = newColor;
+
+//     }
+//     void setTip(String newTip){
+//         tip = newTip;
+//     }
+
+public static void main(String[] args) {
+    student s1 = new student("ankita",52);
+    System.out.println(s1.name);
 }
 
-class Pen{
-    string Color;
-    int Tip;
+static class student{
+    String name;
+    int roll;
 
-    void setColor(String newColor){
-        color = newColor;
-
+    student(String name, int roll) {
+        this.name = name;
+        this.roll = roll;
     }
-    void setTip(String newTip){
-        tip = newTip;
-    }
+}
 }
 
