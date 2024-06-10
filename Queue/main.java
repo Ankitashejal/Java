14977e655069c60858c159f194c48375cba1c4b2
Ankita <Ankitashejal11@gmail.com>
2024-06-10 15:19:17 +0530

    basic operations

--- Queue/main.java
+++ Queue/main.java
@@ -6,12 +6,18 @@ import java.util.Queue;
 public class main {
     public static void main(String[] args) {
         Queue<Integer> queue = new LinkedList<>();
+        System.out.println(queue.isEmpty());
         queue.add(2);
         queue.add(4);
         queue.add(6);
 
         System.out.println(queue);
-        System.out.println(queue.remove());
-        System.out.println(queue.remove());
+        queue.remove();
+        System.out.println(queue);
+        System.out.println(queue.size());
+        // System.out.println(queue.remove());
+       queue.poll(); //removend this are same
+       System.out.println(queue);
+       System.out.println(queue.peek());
     }
 }
